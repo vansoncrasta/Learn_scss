2c69ab927f6202e9d315d71ac34a7a14428adfca
vansoncrasta <vansoncrasta@gmail.com>
2016-05-08 10:25:37 +0530

    Check 02

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('default', function () {
+    gulp.watch('./src/sass/**/*.scss', function (event) {
+        console.log('Watched event :' + event.type + ' for ' + event.path)
+    });
+    gulp.watch('./src/sass/**/*.scss', ['styles']);
 });
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+
+gulp.task('styles', function () {
+    //use gulp.src('./src/sass/**/*.scss',{base:'./src'}) to retain the folder structure.
+    gulp.src('./src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./css/'));
 });
+
+
 
